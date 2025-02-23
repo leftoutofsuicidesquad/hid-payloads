1496d90b0ca710125739aa4bddd7256bcadf7183
Sören Eckhardt <soeren@eckhardt.top>
2025-02-23 16:20:13 +0100

    Create Don Pollo.js

---
+++ Don Pollo.js
@@ -0,0 +1,26 @@
+// Log something to internal console
+console.log("Rickroll via PowerShell");
+
+// Set German keyboard layout
+layout("DE");
+
+// Öffne PowerShell
+press("GUI r");
+delay(500);
+type("powershell");
+press("ENTER");
+delay(1000);
+
+// Piepton abspielen
+console.log("Playing beep sound");
+typingSpeed(0, 0);
+type("$volume = (New-Object -ComObject WScript.Shell); for ($i=0; $i -lt 50; $i++) { $volume.SendKeys([char]175) }");
+press("ENTER");
+delay(1000)
+press("Gui r");
+delay(1000)
+type("https://www.myinstants.com/media/sounds/lingan-guli-wacha.mp3")
+press("ENTER")
+delay(1000)
+type("SPACE")
+console.log("Beep played!");
