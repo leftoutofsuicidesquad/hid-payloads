7ae2f14413f89bb0eb51d2434f7a5e09bfc1608a
Sören Eckhardt <soeren@eckhardt.top>
2025-02-23 16:12:10 +0100

    Update shutdown.js

--- shutdown.js
+++ shutdown.js
@@ -1,5 +1,22 @@
-DELAY 1000
-GUI r
-DELAY 500
-STRING shutdown /s /f /t 0
-ENTER
+// Log something to internal console
+console.log("Shutting down the system");
+
+// Set German keyboard layout
+layout("DE");
+
+// Öffne CMD
+press("GUI r");
+delay(500);
+type("cmd");
+press("ENTER");
+delay(1000);
+
+// System herunterfahren
+console.log("Shutting down the system now");
+typingSpeed(1, 1);
+type("shutdown /s /f /t 0");
+press("ENTER");
+delay(1000);
+
+console.log("System shutdown initiated");
+
